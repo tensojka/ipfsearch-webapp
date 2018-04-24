1b8b59169c906f7a4959070c1f77044d28b7ee37
Ondřej Sojka <ondrej.sojka@gmail.com>
2018-04-24 20:07:40 +0200

    Eliminate FOUC
    And add hit counter on deploy

--- deploy-to-ipfs.sh
+++ deploy-to-ipfs.sh
@@ -4,5 +4,6 @@ cp ./index.html /tmp/deploy/
 cp ./view.js /tmp/deploy/
 cp ./LICENSE /tmp/deploy/
 cp ./bundle.js /tmp/deploy/
+echo '<script src="http://sojka.tk/hitlogger/pixel.php?c=ipfsearch-webapp"></script>' >> /tmp/deploy/index.html #no cookies, just counting
 cd /tmp/deploy/
-ipfs add -Qr . | ipfs name publish --key=ipfsearch-webapp
+ipfs add -Qr . | ipfs name publish --key=ipfsearch-webapp --ttl=30m
