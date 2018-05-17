8bf7e636fdcbd81141e54976a466a245da5c4f8e
--repo <ondrej.sojka@gmail.com>
2018-05-17 19:51:32 +0200

    Polish building

--- deploy-to-ipfs.sh
+++ deploy-to-ipfs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+node_modules/typescript/bin/tsc -p tsconfig.json
 mkdir -p /tmp/deploy
 cp ./index.html /tmp/deploy/
 cp ./view.js /tmp/deploy/
 cp ./LICENSE /tmp/deploy/
 cp ./bundle.js /tmp/deploy/
-echo '<script src="http://sojka.tk/hitlogger/pixel.php?c=ipfsearch-webapp"></script>' >> /tmp/deploy/index.html #no cookies, just counting
 cd /tmp/deploy/
-ipfs add -Qr . | ipfs name publish --key=ipfsearch-webapp --ttl=30m
+ipfs add -Qr . | ipfs name publish --key=ipfsearch-webapp --ttl=1h
