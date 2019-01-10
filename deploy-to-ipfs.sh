5981b60c74010dcc039f9fdbdef6be36ee7c0113
Ondřej Sojka <ondrej.sojka@gmail.com>
2019-01-10 17:50:25 +0100

    Tidy up the text

--- deploy-to-ipfs.sh
+++ deploy-to-ipfs.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 node_modules/typescript/bin/tsc -p tsconfig.json
-mkdir -p /tmp/deploy
+mkdir -p /tmp/deploy/basicresultpage
 cp ./index.html /tmp/deploy/
 cp ./view.js /tmp/deploy/
 cp ./LICENSE /tmp/deploy/
 cp ./bundle.js /tmp/deploy/
+cp ./basicresultpage/* /tmp/deploy/basicresultpage/
 cd /tmp/deploy/
 ipfs add -Qr . | ipfs name publish --key=ipfsearch-webapp --ttl=1h
-scp -qrp . root@ipfsearch.xyz:/www/
-ssh root@ipfsearch.xyz "cat /www/adsnippet >> /www/index.html"
+scp -qrp . root@dev.ipfsearch.xyz:/www/
+ssh root@dev.ipfsearch.xyz "cat /www/adsnippet >> /www/index.html"
